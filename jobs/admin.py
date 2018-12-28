aa103fb5a107b5874a8ff3fc2c6405d6c72471f3
steve <steve@orella.co.uk>
2018-12-28 09:35:52 +0000

    moved old models, tests and admin to oldstuff folder. created new Agent, AgentDetail, Vendor, VendorDetail models in 3NF(?) modified admin to inline the details models in their respective parents

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,28 +2,36 @@ from datetime import datetime
 from django.db import models
 from django.forms import Textarea, TextInput
 from django.contrib import admin
-from .models import Client, Job, Agent, Address, Vendor  # Appointment Job2, Client2, Vendor2
+from .models import Agent, AgentDetail ,Job,Vendor # , Job  # Client, Job, Agent, Address,   # Appointment Job2, Client2, Vendor2
 
 # Register your models here.
 
 admin.site.site_header = 'Estate Agent'
-admin.site.register(Agent)
-admin.site.register(Client)
 
+
+# admin.site.register(Vendor)
+
+
+# admin.site.register(Job)
+
+
+# admin.site.register(Agent)
+# admin.site.register(Client)
 
 class VendorInline(admin.TabularInline):
     model = Vendor
     extra = 1
-    max_num = 1
-    fields = ('name_1', 'phone_1', 'email', 'name_2', 'phone_2', 'phone_3')
-    # show_change_link = True
+    max_num = 3
 
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'name', 'email')
 
-class AddressInline(admin.TabularInline):
-    model = Address
-    extra = 1
-    max_num = 1
-    fields = ('street', 'postcode')
+# class AddressInline(admin.TabularInline):
+#     model = Address
+#     extra = 1
+#     max_num = 1
+#     fields = ('street', 'postcode')
 
 
 # class AppointmentInline(admin.TabularInline):
@@ -35,37 +43,13 @@ class AddressInline(admin.TabularInline):
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    list_display = ['ref', 'get_address', 'get_agent_branch', 'get_appointment', 'photos', 'floorplan']
-    inlines = [VendorInline, AddressInline] # , AppointmentInline]
-    formfield_overrides = {
-            models.CharField: {'widget': TextInput(attrs={'size': '20'})},
-            models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 80})},
-    }
+    list_display = ('ref', 'photos', 'floorplan')
+    inlines = [VendorInline]  #, AddressInline] # , AppointmentInline]
+class AgentDetailsInline(admin.TabularInline):
+    model = AgentDetail
+    extra = 0
 
-    def get_agent_branch(self, obj):
-        try:
-            branch = obj.agent.branch
-        except AttributeError:
-            branch = ""
-        return branch
 
-    get_agent_branch.admin_order_field = 'agent'
-    get_agent_branch.short_description = 'branch'
-
-    def get_address(self, obj):
-        try:
-            address = obj.address_set.all()[0]
-        except IndexError:
-            address = ""
-        return address
-
-    get_address.short_description = "address"
-
-    def get_appointment(self, obj):
-        try:
-            appointment = obj.appointment_set.all()[0]
-        except IndexError:
-            appointment = ""
-        return appointment
-
-    get_appointment.short_description = "appointment"
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    inlines = [AgentDetailsInline]
--- jobs/models.py
+++ jobs/models.py
@@ -3,40 +3,57 @@ from django.contrib.postgres.fields import JSONField
 
 
 # Create your models here.
-
-class Client(models.Model):
-    name_1 = models.CharField(max_length=100, verbose_name='name')
-    name_2 = models.CharField(max_length=100, verbose_name='name', null=True, blank=True)
-    phone_1 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    phone_2 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    phone_3 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    notes = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return str(self.name_1)
-
-
 class Agent(models.Model):
-    branch = models.CharField(max_length=100, verbose_name='branch')
-    name_1 = models.CharField(max_length=100, verbose_name='name', null=True, blank=True)
-    name_2 = models.CharField(max_length=100, verbose_name='name', null=True, blank=True)
-    phone_1 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    phone_2 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    phone_3 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    notes = models.TextField(null=True, blank=True)
-    address = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True, blank=True)
-
-    @property
-    def get_branch(self):
-        return self.branch
+    branch = models.CharField(max_length=50)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    postcode = models.CharField(max_length=8, null=True, blank=True)
 
     def __str__(self):
         return str(self.branch)
 
 
+class AgentDetail(models.Model):
+    phone = models.CharField(max_length=13)
+    name = models.CharField(max_length=13, null=True, blank=True)
+    email = models.CharField(max_length=13, null=True, blank=True)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# class Client(models.Model):
+#     name_1 = models.CharField(max_length=100, verbose_name='name')
+#     name_2 = models.CharField(max_length=100, verbose_name='name', null=True, blank=True)
+#     phone_1 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
+#     phone_2 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
+#     phone_3 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
+#     notes = models.TextField(null=True, blank=True)
+#
+#     def __str__(self):
+#         return str(self.name_1)
+#
+#
+# class Agent(models.Model):
+#     branch = models.CharField(max_length=100, verbose_name='branch')
+#     name_1 = models.CharField(max_length=100, verbose_name='name', null=True, blank=True)
+#     name_2 = models.CharField(max_length=100, verbose_name='name', null=True, blank=True)
+#     phone_1 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
+#     phone_2 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
+#     phone_3 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
+#     notes = models.TextField(null=True, blank=True)
+#     address = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True, blank=True)
+#
+#     @property
+#     def get_branch(self):
+#         return self.branch
+#
+#     def __str__(self):
+#         return str(self.branch)
+
+
 class Job(models.Model):
     ref = models.CharField(max_length=13)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    # client = models.ForeignKey(Client, on_delete=models.CASCADE)
     agent = models.ForeignKey(Agent, on_delete=models.SET_NULL, null=True, blank=True)
     property_type = models.CharField(max_length=20, null=True, blank=True)
     beds = models.SmallIntegerField(null=True, blank=True)
@@ -46,23 +63,23 @@ class Job(models.Model):
     folder = models.CharField(max_length=100, null=True, blank=True)
     specific_reqs = models.TextField(default='Streetscape : 1', null=True, blank=True)
     status = models.BooleanField(default=True, verbose_name='Active')
-    history = models.TextField(null=True, blank=True)
-    appointment = JSONField()
+    # history = models.TextField(null=True, blank=True)
+    # appointment = JSONField()
 
     def __str__(self):
         return str(self.ref)
 
 
-class Address(models.Model):
-    street = models.CharField(max_length=200)
-    postcode = models.CharField(max_length=8)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name_plural = "Address"
-
-    def __str__(self):
-        return f'{self.street}, {self.postcode}'
+# class Address(models.Model):
+#     street = models.CharField(max_length=200)
+#     postcode = models.CharField(max_length=8)
+#     job = models.ForeignKey(Job, on_delete=models.CASCADE)
+#
+#     class Meta:
+#         verbose_name_plural = "Address"
+#
+#     def __str__(self):
+#         return f'{self.street}, {self.postcode}'
 
 
 # class Appointment(models.Model):
@@ -77,14 +94,10 @@ class Address(models.Model):
 
 
 class Vendor(models.Model):
-    name_1 = models.CharField(max_length=100, verbose_name='name')
-    name_2 = models.CharField(max_length=100, verbose_name='name', null=True, blank=True)
-    phone_1 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    phone_2 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
-    phone_3 = models.CharField(max_length=13, verbose_name='phone', null=True, blank=True)
+    phone = models.CharField(max_length=13)
+    name = models.CharField(max_length=100,null=True, blank=True)
     email = models.EmailField(max_length=100, null=True, blank=True)
-    notes = models.TextField(null=True, blank=True)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.name_1)
+        return str(self.name) if self.name else ""
