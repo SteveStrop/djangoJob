d2060d6072ff3c784103bcd1fb1499d4fd21bf51
steve <steve@orella.co.uk>
2018-12-30 10:28:54 +0000

    installed EstateAgent to allow scraping. Added demo scrape to views.index

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from EstateAgent import Scrapers
 
 
 # Create your views here.
 def index(request):
-    pass
+    k = Scrapers.KaScraper()
+    jobs_links = k.extract_job_links()
+    hips = '\n'.join([k.extract_job(l).id for l in jobs_links])
+    k.scraper_close()
+    return HttpResponse(hips)
